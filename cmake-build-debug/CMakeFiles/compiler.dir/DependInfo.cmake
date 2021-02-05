971869aaa7b1778c9860638e44e582374c0fcaf5
hlr <hulingruics@gmail.com>
2021-02-06 01:25:43 +0800

    update

--- cmake-build-debug/CMakeFiles/compiler.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/compiler.dir/DependInfo.cmake
@@ -4,20 +4,20 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/hlr/dev/myproject/compiler/compiler/Compiler.cpp" "/home/hlr/dev/myproject/compiler/cmake-build-debug/CMakeFiles/compiler.dir/compiler/Compiler.cpp.o"
-  "/home/hlr/dev/myproject/compiler/compiler/Options.cpp" "/home/hlr/dev/myproject/compiler/cmake-build-debug/CMakeFiles/compiler.dir/compiler/Options.cpp.o"
-  "/home/hlr/dev/myproject/compiler/main.cpp" "/home/hlr/dev/myproject/compiler/cmake-build-debug/CMakeFiles/compiler.dir/main.cpp.o"
-  "/home/hlr/dev/myproject/compiler/parser/Impl/SimpleLexer.cpp" "/home/hlr/dev/myproject/compiler/cmake-build-debug/CMakeFiles/compiler.dir/parser/Impl/SimpleLexer.cpp.o"
-  "/home/hlr/dev/myproject/compiler/parser/Impl/SimpleToken.cpp" "/home/hlr/dev/myproject/compiler/cmake-build-debug/CMakeFiles/compiler.dir/parser/Impl/SimpleToken.cpp.o"
-  "/home/hlr/dev/myproject/compiler/parser/Impl/TopDownParser.cpp" "/home/hlr/dev/myproject/compiler/cmake-build-debug/CMakeFiles/compiler.dir/parser/Impl/TopDownParser.cpp.o"
-  "/home/hlr/dev/myproject/compiler/sys/Impl/GNUAssembler.cpp" "/home/hlr/dev/myproject/compiler/cmake-build-debug/CMakeFiles/compiler.dir/sys/Impl/GNUAssembler.cpp.o"
-  "/home/hlr/dev/myproject/compiler/sys/Impl/GNUCodeGenerator.cpp" "/home/hlr/dev/myproject/compiler/cmake-build-debug/CMakeFiles/compiler.dir/sys/Impl/GNUCodeGenerator.cpp.o"
-  "/home/hlr/dev/myproject/compiler/sys/Impl/GNULinker.cpp" "/home/hlr/dev/myproject/compiler/cmake-build-debug/CMakeFiles/compiler.dir/sys/Impl/GNULinker.cpp.o"
-  "/home/hlr/dev/myproject/compiler/sys/Impl/X86Linux.cpp" "/home/hlr/dev/myproject/compiler/cmake-build-debug/CMakeFiles/compiler.dir/sys/Impl/X86Linux.cpp.o"
-  "/home/hlr/dev/myproject/compiler/utils/command/CommandInvoker.cpp" "/home/hlr/dev/myproject/compiler/cmake-build-debug/CMakeFiles/compiler.dir/utils/command/CommandInvoker.cpp.o"
-  "/home/hlr/dev/myproject/compiler/utils/command/CommandUtils.cpp" "/home/hlr/dev/myproject/compiler/cmake-build-debug/CMakeFiles/compiler.dir/utils/command/CommandUtils.cpp.o"
+  "/Users/hlr/Inventory/dev/myproject/c_compiler/compiler/Compiler.cpp" "/Users/hlr/Inventory/dev/myproject/c_compiler/cmake-build-debug/CMakeFiles/compiler.dir/compiler/Compiler.cpp.o"
+  "/Users/hlr/Inventory/dev/myproject/c_compiler/compiler/Options.cpp" "/Users/hlr/Inventory/dev/myproject/c_compiler/cmake-build-debug/CMakeFiles/compiler.dir/compiler/Options.cpp.o"
+  "/Users/hlr/Inventory/dev/myproject/c_compiler/main.cpp" "/Users/hlr/Inventory/dev/myproject/c_compiler/cmake-build-debug/CMakeFiles/compiler.dir/main.cpp.o"
+  "/Users/hlr/Inventory/dev/myproject/c_compiler/parser/Impl/SimpleLexer.cpp" "/Users/hlr/Inventory/dev/myproject/c_compiler/cmake-build-debug/CMakeFiles/compiler.dir/parser/Impl/SimpleLexer.cpp.o"
+  "/Users/hlr/Inventory/dev/myproject/c_compiler/parser/Impl/SimpleToken.cpp" "/Users/hlr/Inventory/dev/myproject/c_compiler/cmake-build-debug/CMakeFiles/compiler.dir/parser/Impl/SimpleToken.cpp.o"
+  "/Users/hlr/Inventory/dev/myproject/c_compiler/parser/Impl/TopDownParser.cpp" "/Users/hlr/Inventory/dev/myproject/c_compiler/cmake-build-debug/CMakeFiles/compiler.dir/parser/Impl/TopDownParser.cpp.o"
+  "/Users/hlr/Inventory/dev/myproject/c_compiler/sys/Impl/GNUAssembler.cpp" "/Users/hlr/Inventory/dev/myproject/c_compiler/cmake-build-debug/CMakeFiles/compiler.dir/sys/Impl/GNUAssembler.cpp.o"
+  "/Users/hlr/Inventory/dev/myproject/c_compiler/sys/Impl/GNUCodeGenerator.cpp" "/Users/hlr/Inventory/dev/myproject/c_compiler/cmake-build-debug/CMakeFiles/compiler.dir/sys/Impl/GNUCodeGenerator.cpp.o"
+  "/Users/hlr/Inventory/dev/myproject/c_compiler/sys/Impl/GNULinker.cpp" "/Users/hlr/Inventory/dev/myproject/c_compiler/cmake-build-debug/CMakeFiles/compiler.dir/sys/Impl/GNULinker.cpp.o"
+  "/Users/hlr/Inventory/dev/myproject/c_compiler/sys/Impl/X86Linux.cpp" "/Users/hlr/Inventory/dev/myproject/c_compiler/cmake-build-debug/CMakeFiles/compiler.dir/sys/Impl/X86Linux.cpp.o"
+  "/Users/hlr/Inventory/dev/myproject/c_compiler/utils/command/CommandInvoker.cpp" "/Users/hlr/Inventory/dev/myproject/c_compiler/cmake-build-debug/CMakeFiles/compiler.dir/utils/command/CommandInvoker.cpp.o"
+  "/Users/hlr/Inventory/dev/myproject/c_compiler/utils/command/CommandUtils.cpp" "/Users/hlr/Inventory/dev/myproject/c_compiler/cmake-build-debug/CMakeFiles/compiler.dir/utils/command/CommandUtils.cpp.o"
   )
-set(CMAKE_CXX_COMPILER_ID "GNU")
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
